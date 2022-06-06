8131289c5fb3610262cd0a8c98684b9864b6fad6
makiasake <alexandre.maki@hotmail.com>
2022-06-06 18:29:35 -0300

    Added test profile to instantiate the initial h2 database

--- src/main/java/com/makiasake/basespring2withjwtauthentication/BaseSpring2ProjectWithJwtauthenticationApplication.java
+++ src/main/java/com/makiasake/basespring2withjwtauthentication/BaseSpring2ProjectWithJwtauthenticationApplication.java
@@ -1,34 +1,12 @@
 package com.makiasake.basespring2withjwtauthentication;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import com.makiasake.basespring2withjwtauthentication.domain.UserAccount;
-import com.makiasake.basespring2withjwtauthentication.domain.enums.Profile;
-import com.makiasake.basespring2withjwtauthentication.repositories.UserAccountRepository;
 
 @SpringBootApplication
-public class BaseSpring2ProjectWithJwtauthenticationApplication implements CommandLineRunner {
-
-	@Autowired
-	UserAccountRepository userAccountRepository;
-	
-	@Autowired
-	private BCryptPasswordEncoder pe;
+public class BaseSpring2ProjectWithJwtauthenticationApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BaseSpring2ProjectWithJwtauthenticationApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		
-		UserAccount account = new UserAccount(null, "test@gmail.com", "test", pe.encode("123"));
-		account.addPerfil(Profile.ADMIN);
-		userAccountRepository.save(account);		
-	}
-
 }
---
+++ src/main/java/com/makiasake/basespring2withjwtauthentication/services/DBService.java
@@ -0,0 +1,31 @@
+package com.makiasake.basespring2withjwtauthentication.services;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.makiasake.basespring2withjwtauthentication.domain.UserAccount;
+import com.makiasake.basespring2withjwtauthentication.domain.enums.Profile;
+import com.makiasake.basespring2withjwtauthentication.repositories.UserAccountRepository;
+
+@Service
+public class DBService {
+	
+	@Autowired
+	UserAccountRepository userAccountRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder pe;
+
+	public void instantiateTestDatabase() {
+		UserAccount account = new UserAccount(null, "admin@gmail.com", "test", pe.encode("123"));
+		account.addPerfil(Profile.ADMIN);
+		
+		UserAccount account1 = new UserAccount(null, "user@gmail.com", "test", pe.encode("123"));
+		account1.addPerfil(Profile.USER);
+		
+		userAccountRepository.saveAll(Arrays.asList(account, account1));
+	}
+}
